64995a7208143105f82baf8ae7fa68e971b76443
jks2024 <jks2024@gmail.com>
2022-08-12 11:41:37 +0900

    자동차 정렬 예제 추가

---
+++ src/자동차클래스정렬/CarSortComparator.java
@@ -0,0 +1,15 @@
+package 자동차클래스정렬;
+import java.util.Comparator;
+
+public class CarSortComparator implements Comparator<CarSortInfo> {
+
+    @Override
+    public int compare(CarSortInfo o1, CarSortInfo o2) {
+        return o1.carName.compareTo(o2.carName);
+    }
+
+    @Override
+    public Comparator<CarSortInfo> reversed() {
+        return Comparator.super.reversed();
+    }
+}
